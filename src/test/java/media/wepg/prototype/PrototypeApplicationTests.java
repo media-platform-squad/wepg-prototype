dc85ca2231c9561504303d65cea2932b1bfb9f41
Yebin Lee <yebinlee@Yebinui-MacBookAir.local>
2023-07-18 16:33:30 +0900

    initial commit

---
+++ src/test/java/media/wepg/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package media.wepg.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
