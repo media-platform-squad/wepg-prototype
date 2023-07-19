3a74df873b799c319cc09380d0486343d013c045
ftyebin <yebinlee@Yebinui-MacBookAir.local>
2023-07-19 10:05:23 +0900

    [move]: 통합DB 관련 api를 orig로 경로 및 패키지 변경

--- src/main/java/media/wepg/prototype/orig/repository/ChannelRepository.java
+++ src/main/java/media/wepg/prototype/orig/repository/ChannelRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
+
     List<Channel> findAllBy();
 
     Optional<Channel> findAllById(Long id);
