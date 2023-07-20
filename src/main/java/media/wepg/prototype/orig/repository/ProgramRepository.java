c3ebf813573c36cb9e672f02710c1c20485c547b
ftyebin <i449@sk.com>
2023-07-20 11:04:52 +0900

    [fix]: native query 작성 시 파라미터 추가

--- src/main/java/media/wepg/prototype/orig/repository/ProgramRepository.java
+++ src/main/java/media/wepg/prototype/orig/repository/ProgramRepository.java
@@ -3,6 +3,7 @@ package media.wepg.prototype.orig.repository;
 import media.wepg.prototype.orig.model.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface ProgramRepository extends JpaRepository<Program, Long> {
 
     @Query("select pr from Program pr where pr.eventStartDate >= SYSDATE()-1 and pr.eventEndDate <= SYSDATE()+7 and pr.serviceId = :serviceId")
-    List<Program> findByServiceId(Long serviceId);
+    List<Program> findByServiceId(@Param("serviceId") Long serviceId);
 }
