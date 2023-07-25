c884d5d7d148a55a2c64798da44e226256ed971b
Yebin Lee <lyb2325@gmail.com>
2023-07-26 03:37:29 +0900

    [refactor]: BulkResponseResolver 람다식, early return으로 리팩토링

--- src/main/java/media/wepg/prototype/es/util/BulkResponseResolver.java
+++ src/main/java/media/wepg/prototype/es/util/BulkResponseResolver.java
@@ -1,23 +1,21 @@
 package media.wepg.prototype.es.util;
 
 import co.elastic.clients.elasticsearch.core.BulkResponse;
-import co.elastic.clients.elasticsearch.core.bulk.BulkResponseItem;
 import org.slf4j.Logger;
 
 public class BulkResponseResolver {
     public static void resolveBulkResponse(BulkResponse bulkResponse, Logger logger) {
         if (bulkResponse.errors()) {
             logger.error("Bulk had errors");
-            for (BulkResponseItem item : bulkResponse.items())
+            bulkResponse.items().forEach(item -> {
                 if (item.error() != null) {
                     logger.error(item.error().reason());
                 }
-        } else {
-            logger.info("Bulk Request successful");
-            for (BulkResponseItem item : bulkResponse.items()) {
-                String indexResponse = item.index();
-                logger.info(indexResponse);
-            }
+            });
+            return;
         }
+
+        logger.info("Bulk Request successful");
+        bulkResponse.items().forEach(item -> logger.info(item.index()));
     }
 }
